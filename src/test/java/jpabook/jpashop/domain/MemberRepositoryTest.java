3d5e9e71a8fdf10dad8d15b4137dfdba38daf14e
정동교 <ehdrylang@naver.com>
2021-03-18 23:30:44 +0900

    spring boot + 순수 jpa 조합 repository 테스트

---
+++ src/test/java/jpabook/jpashop/domain/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false) // 처음보는 애노테이션.
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("jdk");
+        //when
+        Long savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedMember);
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+}
